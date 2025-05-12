31385ccf5fe2a11963434251920d95f425f51391
Expert Design Pro <expertdesignpro@gmail.com>
2025-05-12 13:23:09 +0530

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-// import LayoutRoutes from './Routes/routes'
-import HomePage from './Pages/HomePage/home.jsx'
-// import ProjectMindSet from './Pages/ProjectMindSet/index.jsx';
+import LayoutRoutes from './Routes/routes';
+
 
 function App() {
   return (
-    // <LayoutRoutes/>
-    <HomePage/>
-    // <ProjectMindSet/>
+    <LayoutRoutes/>
+    
   );
 }
 
--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,13 +1,15 @@
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";  
-// import ProjectMindSet from '../Pages/ProjectMindSet';
-// import HomePage from '../Pages/HomePage/home'; 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProjectMindSet from '../Pages/ProjectMindSet';
+import HomePage from '../Pages/HomePage/home'; 
 
-// export default function LayoutRoutes() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/project-mindset" element={<ProjectMindSet />} />
-//     </Routes>
-//   );
-// }
+export default function LayoutRoutes() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/project-mindset" element={<ProjectMindSet />} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
