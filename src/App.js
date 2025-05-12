a451ac7d086ff01cc8c274c8ebbba7d5a93883c7
Expert Design Pro <expertdesignpro@gmail.com>
2025-05-12 13:15:47 +0530

    display homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 // import LayoutRoutes from './Routes/routes'
-// import HomePage from './Pages/HomePage/home.jsx'
-import ProjectMindSet from './Pages/ProjectMindSet/index.jsx';
+import HomePage from './Pages/HomePage/home.jsx'
+// import ProjectMindSet from './Pages/ProjectMindSet/index.jsx';
 
 function App() {
   return (
     // <LayoutRoutes/>
-    // <HomePage/>
-    <ProjectMindSet/>
+    <HomePage/>
+    // <ProjectMindSet/>
   );
 }
 
